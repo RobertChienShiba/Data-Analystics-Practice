dde885eb1474fc5a95d1eb95f4d80cdb0b899261
Robert Chien <robertmadhead0919@gmail.com>
2021-08-21 23:34:46 +0800

    2021/8/21

--- crawlers-master/dcard.py
+++ crawlers-master/dcard.py
@@ -13,45 +13,41 @@ import plotly.io as pio
 import plotly.graph_objects as go  
 import pandas as pd
 from collections import defaultdict
-import time
-from selenium import webdriver
-
-
-def crawl_like_data(url):
-    driver=webdriver.Chrome(executable_path="C:\\Users\\rober\\anaconda3\\Lib\\site-packages\\selenium\\webdriver\\chrome\\chromedriver.exe")
-    driver.get(url)
-    
-    like_count=[]  
-    while True:
-        control_stop=input('press enter to continue or -1 to break :')
-        if control_stop != '-1':
-            soup=bs(driver.page_source,'lxml')
-            like_count+=[int(i.text) for i in soup.select('div.yj6g70-1.jsvYtm')[3:]]
-            print(like_count)
-            time.sleep(1)
-        else:
-            break
+import time as t
+import json
         
-    return driver,like_count
+
+def crawl_post(url):  
+    r = requests.get(url)
+    if r.status_code != requests.codes.ok:
+         raise SystemExit # 結束程式
         
+    soup = bs(r.text, 'html.parser')
+        
+    script = soup.find('script', id='__NEXT_DATA__')
+    d = json.loads(script.string) 
+    #pprint(d) 
+    
+    post_id=url.split('/')[-1]    
+    post_data = d['props']['initialState']['post']['data'][post_id] 
+    print('作者:',post_data['authorName']['message'])
+    print('建立時間:',post_data['mediaMeta'][0]['createdAt']) 
+    article=post_data['content'].replace('\r',"").split('\n') 
+    article="\n".join(article)
+    print('文章內容:',article)  
+    print('心情數 :',post_data['reactionCount']) 
+    print('文章分類:',post_data['topics'])
+    comment_count =post_data['commentCount']
+    return comment_count
     
 
-def crawl_article_content(driver):  
-    soup=bs(driver.page_source,'lxml') 
-    
-    article=[i.text.replace('\r',"").split('\n') for i in soup.select('div.phqjxq-0.fQNVmg')[0]]
-    for i in article:
-        article="\n".join(i)
-        print(article)
-    
-
-def crawl_main_data(url): 
+def crawl_comment(url,comment_count):
     com_dict={}
-    time_dict={}
-    page=1
+    like_count=[]
     headers={'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
-             'referer': 'https://www.dcard.tw/','accept-encoding': 'gzip, deflate, br'}
-    while True:
+              'referer': 'https://www.dcard.tw/','accept-encoding': 'gzip, deflate, br'}
+    qualified_post=0
+    for page in range(1, comment_count+1):
         com_resp=requests.get('{}/b/{}'.format(url,page),headers=headers)
         if com_resp.status_code == 200:
             print('連線成功')
@@ -60,16 +56,20 @@ def crawl_main_data(url):
             print('連線失敗')
             break
             
-        soup=bs(com_resp.text,'lxml')
+        soup=bs(com_resp.text,'html.parser')
+        script = soup.select_one('script#__NEXT_DATA__')
+        d = json.loads(script.string)
+        comment_data = d['props']['initialState']['comment']['data']
+        comment_data=comment_data[list(comment_data.keys())[0]]
         try:
-            school=soup.select_one('span.cax7qe-2.enUbOQ').text
+            comment = comment_data['content']
+            like_count.append(comment_data['likeCount'])
+            school=comment_data['school']
             if re.search('.{2,}(大學)$', school) is None:
                 school=None
-            comment=soup.select_one('div.phqjxq-0.fQNVmg').text
             time=soup.select("span.pi0hc4-2.eCtXmy span")[-1].text
         except:
             print('此則留言已被刪除')
-            page+=1
             continue
         if school:
             if school not in com_dict:
@@ -80,10 +80,12 @@ def crawl_main_data(url):
                     com_dict[school][time]=[comment]
                 else:
                     com_dict[school][time].append(comment)
-        page+=1
-        
+            qualified_post+=1  
+        t.sleep(1)
+                    
+    print('擁有完整學校名稱共{}則留言'.format(qualified_post))  
     ##pprint(com_dict) 
-    return com_dict
+    return com_dict , like_count
                 
 def make_dataframe(com_dict,like_count):
     d=[] 
@@ -125,12 +127,12 @@ def visualization(df_com,df_time):
                             marker=dict(line=dict(color='#000000', width=.4)),
                             pull=[0, 0, 0.2, 0], opacity=0.7, rotation=180)
     pie.update_layout(title={'font':{'family':'Courier New','size':40},'text':'學校留言百分佔比',
-                             'x':0.46,'y':.95,'xanchor':'center','yanchor':'top'},
-                             )
+                              'x':0.46,'y':.95,'xanchor':'center','yanchor':'top'},
+                              uniformtext_minsize=5, uniformtext_mode='hide',
+                              )
     pie.show()   
      
-    
-       
+          
     bar=px.bar(df_time,
                 x='留言時間',y=['留言數','按讚數'],
                 barmode='group',opacity=0.9,
@@ -141,19 +143,23 @@ def visualization(df_com,df_time):
                 )
     
     bar.update_layout(title={'font':{'family':'Courier New','size':40},'text':'留言時間長條圖',
-                             'x':0.5,'y':.98,'xanchor':'center','yanchor':'top'},
-                             )
+                              'x':0.5,'y':.98,'xanchor':'center','yanchor':'top'},
+                              )
     
     bar.show()
-
-
-if __name__ == "__main__":
+    
+def main():
     url='https://www.dcard.tw/f/relationship/p/236734422' ##you can change the dcard's url you want to crawl 
-    driver,like_count=crawl_like_data(url)
-    crawl_article_content(driver)
-    com_dict=crawl_main_data(url)
+    comment_count=crawl_post(url)
+    com_dict,like_count=crawl_comment(url,comment_count)
     df_com,df_time=make_dataframe(com_dict,like_count)
     visualization(df_com, df_time)
+
+if __name__ == "__main__":
+    main()
+
+
+
 
         
         
