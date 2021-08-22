c0aecb84b95fe368553f4f4b3235d2638f76c05a
Robert Chien <robertmadhead0919@gmail.com>
2021-08-23 03:00:53 +0800

    2021/8/20

--- selenium-practice/basic.py
+++ selenium-practice/basic.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 
-chromedriver = 'chromedriver.exe'
 
-driver = webdriver.Chrome(chromedriver)
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://www.google.com')
 
-search = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
+search = driver.find_element_by_xpath("//input[@class='gLFyf gsfi']")
 
 search.send_keys('曲面螢幕')
 search.send_keys(Keys.RETURN)
